48af2c5661ee84decd19e7ed197538302087de86
Camilo Otaya <sabastianco@unicauca.edu.co>
2020-10-25 15:43:06 -0500

    Primer commit

---
+++ BuilderRestauranteOnLine/src/main/java/co/edu/unicauca/builder/ClientMain.java
@@ -0,0 +1,26 @@
+package co.edu.unicauca.builder;
+
+/**
+ * Ciente main
+ *
+ * @author Libardo, Julio
+ */
+public class ClientMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DishDirector director = new DishDirector();
+        DishBuilder orientalBuilder = new OrientalDishBuilder();
+        director.setDishBuilder(orientalBuilder);
+        director.buildDish();
+        
+        Dish dish = director.getDish();
+        System.out.println("Oriental dish:" + dish);
+        System.out.println("Sus partes: " + dish.getParts().toString());
+        System.out.println("Precio de las partes: " + dish.calculatePriceParts());
+
+    }
+
+}
